47233a78f8b8c9459eed281e0ef0884198728e5b
Parvesh Dahiya <parveshdahiya@gmail.com>
2023-03-10 10:41:28 +1100

    first commit

---
+++ InvoiceUpdatedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.API.IntegrationEvents.Events;
+
+public record InvoiceUpdatedIntegrationEvent : IntegrationEvent
+{
+    public Invoice OldInvoice { get; private init; }
+    public Invoice NewInvoice { get; private init; }
+
+    public InvoiceUpdatedIntegrationEvent(Invoice oldInvoice, Invoice newInvoice)
+    {
+        OldInvoice = oldInvoice;
+        NewInvoice = newInvoice;
+    }
+}
